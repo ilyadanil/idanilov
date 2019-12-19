aefd285d3685c3e0f3dea910289fc4c95431a9f2
Danilov Ilia <Ilia.Danilov@t-systems.ru>
2019-12-19 22:37:35 +0300

    first commit

---
+++ src/test/java/substringTests.java
@@ -0,0 +1,46 @@
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class substringTests {
+
+    @DataProvider(name = "validSubstrings")
+    public Object[][] indexesAndResult(){
+        return new Object[][]{
+                {5,10, "is a "},
+                {0,1, "T"},
+                {1,1, ""}
+
+        };
+    }
+
+    @DataProvider(name = "negativeSubstrings")
+    public Object[][] indexes(){
+        return new Object[][]{
+                {10,5},
+                {-1,4},
+                {0,Integer.MAX_VALUE},
+                {0,Integer.MAX_VALUE+1}
+        };
+    }
+
+    @BeforeSuite
+    public String setup(){
+        String sourceString = "This is a test string for gett assignment";
+        return sourceString;
+    }
+
+    @Test(dataProvider = "validSubstrings")
+    public void testValidSubstrings(int beginIndex, int endIndex, String result){
+
+        Assert.assertEquals(setup().substring(beginIndex, endIndex), result);
+    }
+
+    @Test(dataProvider = "negativeSubstrings", expectedExceptions = {StringIndexOutOfBoundsException.class},
+            expectedExceptionsMessageRegExp = "String index out of range: .*")
+    public void testNegativeSubstrings(int beginIndex, int endIndex){
+
+        Assert.fail(setup().substring(beginIndex, endIndex));
+    }
+}
